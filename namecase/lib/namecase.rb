8db77b9521e35805512c7cc6df94d4600e379343
aaronp <aaronp@917ac306-ed13-0410-8fcf-ecc17ec2e4de>
2005-11-07 23:24:10 +0000

    Initial revision
    git-svn-id: svn://rubyforge.org/var/svn/namecase/trunk@27 917ac306-ed13-0410-8fcf-ecc17ec2e4de

---
+++ namecase/lib/namecase.rb
@@ -0,0 +1,53 @@
+class NameCase < String
+
+  # Returns a new +String+ with the contents properly namecased
+  def nc
+    localstring = self.downcase
+    localstring.gsub!(/\b\w/) { |first| first.upcase }
+    localstring.gsub!("\'\w\b") { |c| c.downcase } # Lowercase 's
+
+    if localstring =~ /\bMac[A-Za-z]{2,}[^aciozj]\b/ or localstring =~ /\bMc/
+      localstring.gsub!(/\b(Ma?c)([A-Za-z]+)/) { |match| $1 + $2.capitalize }
+
+      # Now fix "Mac" exceptions
+      localstring.gsub!(/\bMacEvicius/, 'Macevicius')
+      localstring.gsub!(/\bMacHado/, 'Machado')
+      localstring.gsub!(/\bMacHar/, 'Machar')
+      localstring.gsub!(/\bMacHin/, 'Machin')
+      localstring.gsub!(/\bMacHlin/, 'Machlin')
+      localstring.gsub!(/\bMacIas/, 'Macias')
+      localstring.gsub!(/\bMacIulis/, 'Maciulis')
+      localstring.gsub!(/\bMacKie/, 'Mackie')
+      localstring.gsub!(/\bMacKle/, 'Mackle')
+      localstring.gsub!(/\bMacKlin/, 'Macklin')
+      localstring.gsub!(/\bMacQuarie/, 'Macquarie')
+    end
+    localstring.gsub!('Macmurdo','MacMurdo')
+
+    # Fixes for "son (daughter) of" etc
+    localstring.gsub!(/\bAl(?=\s+\w)/, 'al')  # al Arabic or forename Al.
+    localstring.gsub!(/\bAp\b/, 'ap')         # ap Welsh.
+    localstring.gsub!(/\bBen(?=\s+\w)/,'ben') # ben Hebrew or forename Ben.
+    localstring.gsub!(/\bDell([ae])\b/,'dell\1')  # della and delle Italian.
+    localstring.gsub!(/\bD([aeiu])\b/,'d\1')   # da, de, di Italian; du French.
+    localstring.gsub!(/\bDe([lr])\b/,'de\1')   # del Italian; der Dutch/Flemish.
+    localstring.gsub!(/\bEl\b/,'el')   # el Greek or El Spanish.
+    localstring.gsub!(/\bLa\b/,'la')   # la French or La Spanish.
+    localstring.gsub!(/\bL([eo])\b/,'l\1')      # lo Italian; le French.
+    localstring.gsub!(/\bVan(?=\s+\w)/,'van')  # van German or forename Van.
+    localstring.gsub!(/\bVon\b/,'von')  # von Dutch/Flemish
+
+    # Fix roman numeral names
+    localstring.gsub!(
+      / \b ( (?: [Xx]{1,3} | [Xx][Ll]   | [Ll][Xx]{0,3} )?
+             (?: [Ii]{1,3} | [Ii][VvXx] | [Vv][Ii]{0,3} )? ) \b /x
+    ) { |match| match.upcase }
+
+    localstring
+  end
+
+  # Modifies _str_ in place and properly namecases the string.
+  def nc!
+    self.gsub!(self, self.nc)
+  end
+end
---
+++ namecase/test/test_namecase.rb
@@ -0,0 +1,44 @@
+$:.unshift File.join(File.dirname(__FILE__), "..", "lib")
+
+require 'namecase'
+require 'test/unit'
+
+class TestNameCase < Test::Unit::TestCase
+  def setup
+    @proper_names = [
+      "Keith",            "Leigh-Williams",       "McCarthy",
+      "O'Callaghan",      "St. John",             "von Streit",
+      "van Dyke",         "Van",          "ap Llwyd Dafydd",
+      "al Fahd",      "Al",
+      "el Grecco",
+      "ben Gurion",   "Ben",
+      "da Vinci",
+      "di Caprio",        "du Pont",              "de Legate",
+      "del Crond",        "der Sind",             "van der Post",
+      "von Trapp",        "la Poisson",           "le Figaro",
+      "Mack Knife",       "Dougal MacDonald",
+      # Mac exceptions
+      "Machin",           "Machlin",              "Machar",
+      "Mackle",           "Macklin",              "Mackie",
+      "Macquarie",        "Machado",              "Macevicius",
+      "Maciulis",         "Macias",               "MacMurdo",
+      # Roman numerals
+      "Henry VIII",       "Louis III",            "Louis XIV",
+      "Charles II",       "Fred XLIX",
+    ]
+  end
+
+  def test_namecase
+    @proper_names.each do |name|
+      nc_name = NameCase.new(name)
+      assert_equal(name, nc_name.downcase.nc)
+    end
+  end
+
+  def test_namecase_modify
+    @proper_names.each do |name|
+      nc_name = NameCase.new(name)
+      assert_equal(name, nc_name.downcase.nc!)
+    end
+  end
+end
